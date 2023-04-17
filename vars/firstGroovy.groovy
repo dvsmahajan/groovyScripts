8c8319e1196729e038f948ba46ca66e15404a5fb
dvsmahajan <dvsmahajan@hotmail.com>
2023-04-17 14:35:33 +0530

    First Commit

---
+++ vars/firstGroovy.groovy
@@ -0,0 +1,15 @@
+pipeline{
+    agent any
+    stages{
+        stage("Start"){
+            steps{
+                println "Staring Stage"
+            }
+        }
+        stage("End"){
+            steps{
+                println "Ending Stage"
+            }
+        }
+    }
+}
