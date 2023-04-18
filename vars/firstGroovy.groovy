680ca1ac288b9cd8180541d7a4aa524ee4c6e111
dvsmahajan <dvsmahajan@hotmail.com>
2023-04-18 21:08:11 +0530

    adding body parameter in firstGroovy script

--- vars/firstGroovy.groovy
+++ vars/firstGroovy.groovy
@@ -14,22 +14,24 @@ def call(body){
             stage("Start"){
                 steps{
                     println "Staring Stage"
-                  }
+                    config['stageName'] = "Running Stage"
+                }
             }
             stage("Running"){
                 steps{
-                    println "$config"
+                    println "$config['stageName']"
+                    config['stageName'] = "Pause"
                 }
             }
             stage("Pause"){
                 steps{
-                    println "$config"
-                
+                    println "$config['stageName']"
+                    config['stageName'] = "End"
                 }
             }
             stage("End"){
                 steps{
-                    println "$config"
+                    println "$config['stageName']"
                 }
             }
         }
