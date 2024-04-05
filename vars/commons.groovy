80304013965d29d304b1a94e76bc8667716f2566
spoorthysoma <spoorthyvsoma123@gmail.com>
2024-04-05 21:13:13 +0530

    adding new file

---
+++ vars/commons.groovy
@@ -0,0 +1,43 @@
+import org.jenkinsci.plugins.pipeline.modeldefinition.Utils
+import groovy.lang.Binding
+import groovy.lang.GroovyShell
+import groovy.transform.Field
+
+
+def call(body){
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    def all_params = [:]
+    pipeline{
+        agent any
+        stages{
+            stage("Start"){
+                steps{
+                    println "Staring Stage"
+                    
+                }
+            }
+            stage("Running"){
+                steps{
+                    println "$config"
+                    
+                }
+            }
+            stage("Pause"){
+                steps{
+                    println "$config"
+                    
+                }
+            }
+            stage("End"){
+                steps{
+                    println "$config.name"
+                    config.name = "Done"
+                    println "$config.name"
+                }
+            }
+        }
+    }
+}
