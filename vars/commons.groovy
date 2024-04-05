a9348dd4a05905e76cb9582a0a6c3ba93f597c23
dvsmahajan <spoorthyvsoma123@gmail.com>
2024-04-05 22:58:24 +0530

    modified the commons groovy file

--- vars/commons.groovy
+++ vars/commons.groovy
@@ -3,38 +3,39 @@ import groovy.lang.Binding
 import groovy.lang.GroovyShell
 import groovy.transform.Field
 
-def call(Closure body) {
-    def config = [:] // Initialize an empty map
-
+def call(body) {
+     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
-
-    pipeline {
+    def all_params = [:]
+    pipeline{
         agent any
-        stages {
-            stage("Start") {
-                steps {
-                    echo "Starting Stage"
+        stages{
+            stage("Start"){
+                steps{
+                    println "Staring Stage"
+                    
                 }
             }
-            stage("Running") {
-                steps {
-                    echo "$config"
+            stage("Running"){
+                steps{
+                    println "$config"
+                    
                 }
             }
-            stage("Pause") {
-                steps {
-                    echo "$config"
+            stage("Pause"){
+                steps{
+                    println "$config"
+                    
                 }
             }
-            stage("End") {
-                steps {
-                    echo "$config.name"
-                    // config.name = "Done"
-                    echo "$config.name"
+            stage("End"){
+                steps{
+                    println "$config.name"
+                    config.name = "Done"
+                    println "$config.name"
                 }
             }
         }
     }
-}
