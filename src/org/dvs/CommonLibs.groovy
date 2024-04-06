6ae51ce493f3de4d5d72f1e3467e199da028640f
dvsmahajan <spoorthyvsoma123@gmail.com>
2024-04-07 00:03:35 +0530

    modified

--- src/org/dvs/CommonLibs.groovy
+++ src/org/dvs/CommonLibs.groovy
@@ -18,8 +18,9 @@ def execute(config){
             sshagent(['masterSSHID']){
 
                 sh """
-                    echo "I am trying to connect the server"
-                    ssh  -i "$SSH_KEY"  -o StrictHostKeyChecking=no user@192.168.0.102 "pwd;ls -a;" 
+                    echo "I am trying to connect the server";
+                    pwd;
+                    ls -a; 
                     """
             }
         }
