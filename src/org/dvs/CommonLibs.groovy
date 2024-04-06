3513fdc1174cbf15375813726fb7925d1c3ad90a
dvsmahajan <spoorthyvsoma123@gmail.com>
2024-04-06 23:20:44 +0530

    modified

--- src/org/dvs/CommonLibs.groovy
+++ src/org/dvs/CommonLibs.groovy
@@ -14,6 +14,14 @@ def execute(config){
                     mvn clean install;
                 """
         }
+        stage('validate server'){
+            withCredentials([sshUserPrivateKey(credentialsId: 'masterID')]) {
+                ssh """
+                    echo "I am trying to connect the server"
+                    ssh user@192.168.0.102 "pwd;ls -a;" 
+                    """
+            }
+        }
     }catch (err){
         printf(err)
     }
