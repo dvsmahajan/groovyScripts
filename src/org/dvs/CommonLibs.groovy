97eefd89c84171172dcc8b31a99710537dd7619a
dvsmahajan <spoorthyvsoma123@gmail.com>
2024-04-06 23:31:18 +0530

    modified

--- src/org/dvs/CommonLibs.groovy
+++ src/org/dvs/CommonLibs.groovy
@@ -7,15 +7,15 @@ def pipelineJob(config){
 def execute(config){
     print(config)
     try {
-        stage("build"){
-            sh """
-                    pwd;
-                    ls -a;
-                    mvn clean install;
-                """
-        }
+//        stage("build"){
+//            sh """
+//                    pwd;
+//                    ls -a;
+//                    mvn clean install;
+//                """
+//        }
         stage('validate server'){
-            withCredentials([sshUserPrivateKey(credentialsId: 'masterID')]) {
+            withCredentials([string(credentialsId: 'masterID', variable: 'PASSWORD')]) {
                 ssh """
                     echo "I am trying to connect the server"
                     ssh user@192.168.0.102 "pwd;ls -a;" 
