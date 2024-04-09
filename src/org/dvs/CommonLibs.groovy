8bd603b815578e6664ee972b545a5eca6e481f75
dvsmahajan <spoorthyvsoma123@gmail.com>
2024-04-09 14:19:45 +0530

    modified

--- src/org/dvs/CommonLibs.groovy
+++ src/org/dvs/CommonLibs.groovy
@@ -7,22 +7,46 @@ def pipelineJob(config){
 def execute(config){
     print(config)
     try {
-//        stage("build"){
-//            sh """
-//                    pwd;
-//                    ls -a;
-//                    mvn clean install;
-//                """
-//        }
-        stage('validate server'){
-            sshagent(['masterSSHID']){
+        isDeployment = config['deployment'];
+        isService = config['service'];
+        isDocker = config['docker'];
+        isDeploy = config['deploy'];
 
-                sh """
+        if(isDeploy){
+            stage('Prepare'){
+                sshagent(['masterSSHID']){
+
+                    sh """
                     echo "I am trying to connect the server"
                     ssh  -o StrictHostKeyChecking=no user@192.168.0.102 "rm -rf /home/user/app ; mkdir /home/user/app ; cd /home/user/app; pwd ; ls -a" 
                     """
+                }
+            }
+            stage('Build'){
+                sh "mvn clean install"
+            }
+
+            stage('Deploy'){
+
+                print("Deploying the application in server")
+
+                sh """sshpass -p "user" scp -o PreferredAuthentications="password"  target/eureka-1.war user@192.168.0.102:/home/user/app/; """
+
+                print("Deploying the deployement yaml in server")
+
+                if(isDeployment){
+                    sh """sshpass -p "user" scp -o PreferredAuthentications="password" doployment.yaml  target/eureka-1.war user@192.168.0.102:/home/user/app/;"""
+                }
+
+                print("Deploying the Service yaml in server")
+
+                if(isService){
+                    sh """sshpass -p "user" scp -o PreferredAuthentications="password" service.yaml   user@192.168.0.102:/home/user/app/;"""
+
+                }
             }
         }
+
     }catch (err){
         print(err)
         throw err;
