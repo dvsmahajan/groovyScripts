3cf565d0050d4f48fbd5f46698d49abc765f49fb
dvsmahajan <spoorthyvsoma123@gmail.com>
2024-04-10 17:00:19 +0530

    modified CommonLibs groovy

--- src/org/dvs/CommonLibs.groovy
+++ src/org/dvs/CommonLibs.groovy
@@ -86,33 +86,33 @@ def execute(config){
     }
 }
 
-buildMavenProject(appName){
+def buildMavenProject(appName){
     sh "pwd; ls -a"
     sh "cd $WORKSPACE; cd $appName; mvn clean install"
 }
 
-cloneRepo(appName,repoUrl){
+def cloneRepo(appName,repoUrl){
     script {
         sh 'cd $WORKSPACE; rm -rf $appName; git clone $repoUrl ; cd $appName;'
     }
 }
 
-copyJarToServer(appName,jarName){
+def copyJarToServer(appName,jarName){
     sh """sshpass -p "user" scp -o PreferredAuthentications="password"  $appName/target/$jarName-1.war user@192.168.0.102:/home/user/app/; """
 }
 
-copyFile(path, destinationPath){
+def copyFile(path, destinationPath){
     sh """sshpass -p "user" scp -o PreferredAuthentications="password"  $path $destinationPath """
 }
 
-buildDockerImage(jarName, deployIp){
+def buildDockerImage(jarName, deployIp){
     sh """
     echo "I am trying to connect the server"
     ssh  -o StrictHostKeyChecking=no user@$deployIp " cd /home/user/app; echo 'Trying to create docker image'; docker build -t $jarName . ; docker image tag $jarName  localhost:5000/$jarName; docker push localhost:5000/eureka" 
     """
 }
 
-applyKubernates(serverIp, file){
+def applyKubernates(serverIp, file){
     sh """
         ssh  -o StrictHostKeyChecking=no user@$serverIp " cd /home/user/app; kubectl apply -f $file" 
     """
